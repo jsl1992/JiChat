e35f7b92a9367e83635962c918a3eb8bfefc0805
jisl <1162568294@qq.com>
2024-01-21 14:32:37 +0800

    mybatis

--- jichat-modules/user-service/user-service-app/src/test/java/com/ji/jichat/user/MybatisCodeGeneratorTest.java
+++ jichat-modules/user-service/user-service-app/src/test/java/com/ji/jichat/user/MybatisCodeGeneratorTest.java
@@ -16,7 +16,7 @@ public class MybatisCodeGeneratorTest {
     private static final String userName = "root";
     private static final String password = "root";
     //需要生成的表(多张表用","分隔,例如:"t_user,t_role")
-    private static final String tableNames = "t_user,t_device";
+    private static final String tableNames = "t_user,t_device,roles";
     //模块地址
     private static final String module_path = "/jichat-modules/user-service/user-service-app";
     //包名
