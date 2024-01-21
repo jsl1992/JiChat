53ada8b235ca64a05a10d3fb6d302967eea4e54c
jisl <1162568294@qq.com>
2024-01-21 14:34:10 +0800

    mybatis

--- jichat-framework/jichat-spring-boot-starter-mybatis/src/main/java/com.ji.jichat.mybatis/MybatisCodeGenerator.java
+++ jichat-framework/jichat-spring-boot-starter-mybatis/src/main/java/com.ji.jichat.mybatis/MybatisCodeGenerator.java
@@ -9,6 +9,22 @@ import java.util.Collections;
  * @author jishenglong on 2023/2/16 9:05
  */
 public class MybatisCodeGenerator {
+
+    //示例值
+    //作者
+    private static final String author = "jisl";
+
+    //数据库
+    private static final String url = "jdbc:mysql://192.168.137.130:3306/jichat_user?useUnicode=true&characterEncoding=utf8&zeroDateTimeBehavior=convertToNull&useSSL=true&serverTimezone=GMT%2B8";
+    private static final String userName = "root";
+    private static final String password = "root";
+    //需要生成的表(多张表用","分隔,例如:"t_user,t_role")
+    private static final String tableNames = "t_user,t_device,roles";
+    //模块地址
+    private static final String module_path = "/jichat-modules/user-service/user-service-app";
+    //包名
+    private static final String package_name = "com.ji.jichat.user";
+
     /**
      * 代码生成
      *
@@ -43,6 +59,10 @@ public class MybatisCodeGenerator {
                 })
 //                .templateEngine(new FreemarkerTemplateEngine()) // 使用Freemarker引擎模板，默认的是Velocity引擎模板
                 .execute();
+    }
+
+    public static void main(String[] args) {
+        MybatisCodeGenerator.generator(url, userName, password, author, module_path, package_name, tableNames);
     }
 
 
