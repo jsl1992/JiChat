be117a8c2cd8c40c54dee7d79c03bfc664d1889e
jisl <1162568294@qq.com>
2024-10-31 17:20:27 +0800

    jdk8版本框架

--- jichat-modules/chat-service/chat-service-app/src/test/java/com/ji/jichat/chat/NacosServiceListener.java
+++ jichat-modules/chat-service/chat-service-app/src/test/java/com/ji/jichat/chat/NacosServiceListener.java
@@ -8,7 +8,7 @@ import com.alibaba.nacos.api.naming.listener.NamingEvent;
 import java.util.Properties;
 
 /**
-  * 监听nacos服务变化
+  * 监听nacos服务变化 jdk8版本
  * 注册服务方法 post:http://192.168.137.130:8848/nacos/v1/ns/instance?serviceName=nacos.naming.serviceName&ip=20.18.7.10&port=8080
   * @author jisl on 2024/1/17 13:58
   **/
