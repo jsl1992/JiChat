d163a05c5e2397d1fd8a822a43aa5e1b9e5d00cc
jisl <1162568294@qq.com>
2025-02-06 15:04:32 +0800

    excel

--- jichat-framework/jichat-spring-boot-starter-excel/src/main/java/com/ji/jichat/excel/util/service/ExcelExportService.java
+++ jichat-framework/jichat-spring-boot-starter-excel/src/main/java/com/ji/jichat/excel/util/service/ExcelExportService.java
@@ -1,5 +1,6 @@
 package com.ji.jichat.excel.util.service;
 
+import cn.hutool.core.date.StopWatch;
 import com.alibaba.excel.EasyExcel;
 import com.alibaba.excel.ExcelWriter;
 import com.alibaba.excel.write.metadata.WriteSheet;
@@ -7,19 +8,37 @@ import com.github.pagehelper.ISelect;
 import com.ji.jichat.common.pojo.PageDTO;
 import com.ji.jichat.common.pojo.PageVO;
 import com.ji.jichat.mybatis.util.JiPageHelper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.TimeUnit;
+
 @Service
+@Slf4j
 public class ExcelExportService<T> {
 
-    // 每次查询的默认分页大小
-    private static final int DEFAULT_PAGE_SIZE = 2;
+    /**
+     * 每次查询的默认分页大小
+     **/
+    private static final int DEFAULT_PAGE_SIZE = 3000;
 
-    // 通用的导出方法
+    /**
+     * 导出excel
+     *
+     * @param fileName       导出文件名
+     * @param query          导出的查询方法
+     * @param dataModelClass 导出返回实体类
+     * @author jisl on 2025/2/6 14:58
+     **/
     public void exportToExcel(String fileName, ISelect query, Class<T> dataModelClass) {
         // 获取总记录数
+        final StopWatch stopWatch = new StopWatch("导出开始:" + fileName);
+        stopWatch.start("总数查询");
         final long totalRecords = JiPageHelper.doSelectPageInfo(new PageDTO(1, DEFAULT_PAGE_SIZE), query).getTotal();
         final long totalPages = (totalRecords + DEFAULT_PAGE_SIZE - 1) / DEFAULT_PAGE_SIZE;
+        log.info("导出totalRecords=%s,totalPages=%s", totalRecords, totalPages);
+        stopWatch.stop();
+        stopWatch.start("分页导出");
         // 创建 ExcelWriter 对象
         final ExcelWriter excelWriter = EasyExcel.write(fileName).build();
         // 分页查询并写入数据
@@ -32,5 +51,7 @@ public class ExcelExportService<T> {
         }
         // 完成写入
         excelWriter.finish();
+        stopWatch.stop();
+        log.info("导出用时:" + stopWatch.prettyPrint(TimeUnit.MILLISECONDS));
     }
 }
--- jichat-modules/user-service/user-service-app/src/test/java/com/ji/jichat/user/service/ExcelExportServiceTest.java
+++ jichat-modules/user-service/user-service-app/src/test/java/com/ji/jichat/user/service/ExcelExportServiceTest.java
@@ -1,11 +1,15 @@
 package com.ji.jichat.user.service;
 
+import cn.hutool.core.util.RandomUtil;
 import com.ji.jichat.excel.util.service.ExcelExportService;
+import com.ji.jichat.user.api.dto.SystemSmsCodeDTO;
 import com.ji.jichat.user.api.dto.SystemSmsCodePageDTO;
 import com.ji.jichat.user.excel.SystemSmsCodeExcelVO;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Date;
 
 @SpringBootTest
 public class ExcelExportServiceTest {
@@ -22,6 +26,19 @@ public class ExcelExportServiceTest {
         ExcelExportService excelExportService=new ExcelExportService();
         final SystemSmsCodePageDTO dto = SystemSmsCodePageDTO.builder().build();
         excelExportService.exportToExcel("C:\\Newand\\file\\导出测试.xlsx", () -> systemSmsCodeService.page(dto), SystemSmsCodeExcelVO.class);
+    }
+
+
+    @Test
+    public void add() {
+        for (int i=0;i<100000;i++){
+            final SystemSmsCodeDTO dto = SystemSmsCodeDTO.builder()
+                    .mobile("1"+RandomUtil.randomNumbers(10)).code(RandomUtil.randomNumbers(6))
+                    .createIp("192.168.137.88").scene(RandomUtil.randomInt(6)).todayIndex(1)
+                    .used(1).usedTime(new Date()).usedIp("192.168.137.66")
+                    .build();
+            systemSmsCodeService.add(dto);
+        }
     }
 
 
