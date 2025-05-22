1f23421ef5fa23a139a23ae739d1bc9e7c4c4b1c
jisl <1162568294@qq.com>
2025-05-22 11:23:02 +0800

    添加好友单元测试

--- jichat-modules/user-service/user-service-app/src/test/java/com/ji/jichat/user/test/UserServiceImplTest.java
+++ jichat-modules/user-service/user-service-app/src/test/java/com/ji/jichat/user/test/UserServiceImplTest.java
@@ -72,7 +72,7 @@ class UserServiceImplTest {
     @Test
     @Rollback(false)
         // 禁用回滚
-    void test() {
+    void testAddFriend() {
         final List<User> list = userService.list();
         for (int i = 0; i < list.size(); i++) {
             for (int j = i + 1; j < list.size(); j++) {
